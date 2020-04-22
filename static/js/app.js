9b93eed0e2a342376bcb912a90c18c27f9590c99
TudorDan <tudor.ist@gmail.com>
2020-04-22 15:13:09 +0300

    Add connection to cards table

--- static/js/app.js
+++ static/js/app.js
@@ -3,16 +3,17 @@ url_boards = url_api + "get-boards";
 url_cards = url_api + "get-cards/";
 
 
-document.addEventListener("DOMContentLoaded",createBoards)
+document.addEventListener("DOMContentLoaded", createBoards);
 
 function createBoards() {
     let button = document.querySelector('#sendBoardTitle');
     button.addEventListener('click', (e) => {
         let title = document.querySelector('#title').value;
-        let titleDict = { 'title': title }
+        let titleDict = {'title': title}
         sendBoard(titleDict);
     });
 }
+
 const sendBoard = async (data) => {
     const location = window.location + 'api/create-board'
     const setting =
@@ -46,71 +47,128 @@ function getCardsForBoard(id) {
         })
         .then((jsonResponse) => {
             console.table(jsonResponse);
+            showCards(jsonResponse);
         });
 
 }
 
-function showBoards(boards) {
-    for (board of boards) {
-        boards_container = document.getElementById('boards-container');
-        card_template = `<div class="card mb-2">
+function showCards(cards) {
+    for (let card of cards) {
+        let table_header = document.querySelector('.tableHead-' + card.board_id);
+        let len = table_header.children.length;
+        let card_container = document.querySelector('.tablebody-' + card.board_id);
+        card_container.innerHTML = '';
+        let tr = '';
+        for (let i = 1; i <= len; i++) {
+            if (parseInt(card.status_id) === i) {
+                tr += `<td>${card.card_title}</td>`;
+            } else {
+                tr += `<td></td>`;
+            }
+        }
+        let card_template = `
+        <tr>
+            ${tr}
+        </tr>
+        `;
+        card_container.innerHTML += card_template;
+    }
+}
+
+    function addCardsToColumn(container) {
+        const cardElement = createCard(
+            'New',
+            'Task 1');
+        document.querySelector(`#${container}`).appendChild(cardElement);
+    }
+
+    const createCard = function (title, text) {
+        const container = document.createElement('div');
+        container.classList.add('card');
+
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+        const cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = title;
+        cardBody.appendChild(cardTitle);
+
+        const cardText = document.createElement('p');
+        cardText.classList.add('card-text');
+        cardText.textContent = text;
+        cardBody.appendChild(cardText);
+
+        container.appendChild(cardBody);
+
+        return container;
+    };
+
+    function showBoards(boards) {
+        for (board of boards) {
+            boards_container = document.getElementById('boards-container');
+            card_template = `<div class="card mb-2">
         <div class="card-body" >
         <h5 class="card-title" id="board-title-` + board.id + `" contenteditable="true" onfocusout="updateBoardTitle(` + board.id + `)">` + board.title + `</h5>
-        <a href="#collapseExample`+ board.id + `" role="button" aria-expanded="false" aria-controls="collapseExample`+ board.id + `"
+        <a href="#collapseExample` + board.id + `" role="button" aria-expanded="false" aria-controls="collapseExample` + board.id + `"
          class="showcards" data-toggle="collapse" onClick="getCardsForBoard(` + board.id + `) ">▼</a>
         </div>
-        <div class="collapse" id="collapseExample`+ board.id + `">
+        <div class="collapse" id="collapseExample` + board.id + `">
         <button type="button" class="btn btn-success btn-sm ml-5 float-left" onclick="newColumn(${board.id})">Add Column</button><br><br>
          <div class="card card-body mb-5" >
-            <table class="table table-bordered">
-            <thead >
-            <tr class="tableHead-${board.id}">
-            </tr>
-            </thead>
+            <button type="button" class="btn btn-outline-success taskButton">Add Card</button>
+            <table class="table table-bordered table-dark">
+                <thead >
+                    <tr class="tableHead-${board.id}">
+                        <th>New</th>
+                        <th>In progress</th>
+                        <th>Testing</th>
+                        <th>Done</th>
+                    </tr>
+                </thead>
+                <tbody class="tablebody-${board.id}"></tbody>
             </table>
         </div>
         </div>
         </div>`
-        boards_container.innerHTML += card_template;
-    }
-}
-
-function newColumn (boardid) {
-    let tablehead = document.querySelector(".tableHead-" + boardid)
-    console.log(tablehead)
-    let th = `<th class="col" contenteditable="true" style="width: 25%"> New Column</th>`
-    tablehead.innerHTML += th;
-}
-
-
-function updateBoardTitle(boardId) {
-    elementToSelect = "board-title-" + boardId;
-    titleValue = document.getElementById(elementToSelect);
-
-    data = {
-        'id': boardId,
-        'title': titleValue.innerText,
+            boards_container.innerHTML += card_template;
+        }
     }
 
-    settings = {
-        'method': 'POST',
-        'headers': {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify(data),
+    function newColumn(boardid) {
+        let tablehead = document.querySelector(".tableHead-" + boardid)
+        console.log(tablehead)
+        let th = `<th class="col" contenteditable="true" style="width: 25%">Type status</th>`
+        tablehead.innerHTML += th;
     }
 
-    fetch('/api/update-board', settings)
-        .then((serverResponse) => {
-            return serverResponse.json();
-        })
-        .then((jsonResponse) => {
-            console.log(jsonResponse);
-        })
-}
+
+    function updateBoardTitle(boardId) {
+        elementToSelect = "board-title-" + boardId;
+        titleValue = document.getElementById(elementToSelect);
+
+        data = {
+            'id': boardId,
+            'title': titleValue.innerText,
+        }
+
+        settings = {
+            'method': 'POST',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(data),
+        }
+
+        fetch('/api/update-board', settings)
+            .then((serverResponse) => {
+                return serverResponse.json();
+            })
+            .then((jsonResponse) => {
+                console.log(jsonResponse);
+            })
+    }
 
 
-
-
-getBoards();
+    getBoards();
